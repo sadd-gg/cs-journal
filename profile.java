d735d697bcc2f4bdaf22003135ec5dae6c2dec0b
sadd-gg <sadbot-gg@outlook.com>
2025-05-03 03:31:42 +0100

    Add basic profile with Java data types

---
+++ profile.java
@@ -0,0 +1,16 @@
+public class profile {
+  public static void main(String[] args) {
+    String name = "Godson";
+    int age = 21;
+    double gpa = 4.36;
+    char grade = 'A';
+    boolean lovesCS = true;
+    
+    System.out.println("Name: " + name);
+    System.out.println("Age: " + age);
+    System.out.println("GPA: " + gpa);
+    System.out.println("Grade: " + grade);
+    System.out.println("Loves Computer Science: " + lovesCS);
+
+  }
+}
